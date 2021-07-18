942af848459c12dc96941aaf6296bfd8627d593d
alulet <aalettera3@gmail.com>
2021-07-18 20:02:40 -0300

    tercer commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer'
+import React, { useState } from 'react';
+import ItemCount from './components/ItemCount';
+
 
 
 function App() {
+  const [stock, setStock] = useState(0);
   return (
     <div className="App">
      <NavBar />
      <ItemListContainer nombre="Bienvenido"/>
+     <ItemCount stock={5} initial={1}/>
      </div>
   );
 }
