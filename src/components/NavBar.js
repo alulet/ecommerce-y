ff992dbd036adfec8b10f968fdef1ed5acfafe9a
alulet <aalettera3@gmail.com>
2021-07-18 20:11:40 -0300

    tercer commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,27 +3,27 @@ import CartWidget from "./CartWidget";
 function NavBar() {
   return (
     <div className="">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Tienda de Alfajores</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a classNames="navbar-brand" href="#">Tienda de Alfajores</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Nosotros</a>
+      <li className="nav-item">
+        <a classNames="nav-link" href="#">Nosotros</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <li className="nav-item dropdown">
+        <a claclassNamess="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Alfajores
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Dulce de Leche</a>
-          <a class="dropdown-item" href="#">Sin Tacc</a>
-          <a class="dropdown-item" href="#">Veganos</a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a className="dropdown-item" href="#">Dulce de Leche</a>
+          <a className="dropdown-item" href="#">Sin Tacc</a>
+          <a className="dropdown-item" href="#">Veganos</a>
         </div>
       </li>
     </ul>
